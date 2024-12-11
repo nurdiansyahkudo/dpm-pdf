a3016067abeafab35030e98a05f276bf8ebcfe37
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-11 18:54:57 +0700

    Add Line Number

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -19,9 +19,10 @@ class StockMove(models.Model):
 
     line_no = fields.Integer(string="No.", compute="_compute_line_no", store=True)
 
-    @api.depends('move_line_ids')
+    @api.depends('picking_id.move_ids')
     def _compute_line_no(self):
         for move in self:
-            move_lines = move.move_line_ids
-            for index, line in enumerate(move_lines, start=1):
-                line.move_id.line_no = index
+            if move.picking_id:  # Pastikan move memiliki picking_id
+                move_list = move.picking_id.move_ids.sorted('id')  # Sortir untuk konsistensi
+                for index, line in enumerate(move_list, start=1):
+                    line.line_no = index
