dcfe6e75614c4b6b08537c7e160b8a3202921c00
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-11 18:27:34 +0700

    Add Line Number

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,7 +4,7 @@ class Picking(models.Model):
     _inherit = "stock.picking"
 
     total_qty = fields.Float(string="Total Quantity", compute="_compute_total_qty", store=True)
-    
+
     @api.depends('move_ids_without_package.quantity')
     def _compute_total_qty(self):
         for picking in self:
@@ -13,3 +13,15 @@ class Picking(models.Model):
 
     def _get_print_report_name(self):
         return 'DPM Delivery Order - %s' % (self.name)
+    
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    line_no = fields.Integer(string="No.", compute="_compute_line_no", store=True)
+
+    @api.depends('picking_id')
+    def _compute_line_no(self):
+        for picking in self.mapped('picking_id'):
+            move_lines = picking.move_ids_without_package.mapped('move_line_ids')
+            for index, line in enumerate(move_lines, start=1):
+                line.line_no = index
