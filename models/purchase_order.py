de3a576a6165e1c229f314b2fb6596a12c70bb73
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-11 14:20:16 +0700

    Add Line Number

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,10 +1,19 @@
 from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
-  _inherit = "purchase.order"
+    _inherit = "purchase.order"
 
-  def get_print_report_name(self):
+    total_qty = fields.Float(string="Total Quantity", compute="_compute_total_qty", store=True)
+
+    def get_print_report_name(self):
         return 'DPM Purchase Order - %s' % (self.name)
+
+    @api.depends('order_line.product_qty')
+    def _compute_total_qty(self):
+        for order in self:
+            total = sum(line.product_qty for line in order.order_line)
+            order.total_qty = total
+
 class PurchaseOrderLine(models.Model):
   _inherit = "purchase.order.line"
 
