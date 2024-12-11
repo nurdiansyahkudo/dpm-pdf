4a3d97ab36bce2b625df97c055347bdf86acc7da
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-11 17:26:42 +0700

    Add Line Number

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -14,8 +14,8 @@ class Picking(models.Model):
           total = sum(line.quantity for line in move.move_ids_without_package)
           move.total_qty = total
 
-class StockMove(models.Model):
-  _inherit = "stock.move"
+class StockMoveLine(models.Model):
+  _inherit = "stock.move.line"
 
   line_no = fields.Integer(string="No.", compute="_compute_line_no", store=True)
   
