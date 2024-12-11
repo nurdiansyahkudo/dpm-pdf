58d1a5ac9ff2020ad835689fadefabda57d732e4
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-11 17:36:32 +0700

    Add Line Number

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -15,8 +15,8 @@ class Picking(models.Model):
           total = sum(line.quantity for line in move.move_line_ids)
           move.total_qty = total
 
-  @api.depends('move_line_ids')
+  @api.depends('move_id')
   def _compute_line_no(self):
-      for move in self.mapped('move_line_ids'):
-          for index, line in enumerate(move.move_line_ids, start=1):
+      for move in self.mapped('move_id'):
+          for index, line in enumerate(move.move_id, start=1):
               line.line_no = index
