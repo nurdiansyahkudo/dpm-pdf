15098ea0940a1c821a0f3be17963ef55fd8f8d6d
nurdiansyahkudo <nurdiansyah192@gmail.com>
2024-12-11 17:55:24 +0700

    Add Line Number

--- models/stock_move.py
+++ models/stock_move.py
@@ -1,15 +1,17 @@
 from odoo import models, fields, api
 
-class StockMoveLine(models.Model):
-    _inherit = "stock.move.line"
-
-    line_no = fields.Integer(string="No.", compute="_compute_line_no", store=True)
-
 class StockMove(models.Model):
     _inherit = "stock.move"
+
+    line_no = fields.Integer(string="No.", compute="_compute_line_no", store=True)
 
     @api.depends('move_line_ids')
     def _compute_line_no(self):
         for move in self:
             for index, line in enumerate(move.move_line_ids, start=1):
                 line.line_no = index
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    line_no = fields.Integer(string="No.")
